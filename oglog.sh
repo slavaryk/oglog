c160adbf367cce09c254dd1c1e38e6cb27042245
rykov <m3atyy@gmail.com>
2022-08-12 17:35:31 +0500

    adds pieces menu

--- oglog.sh
+++ oglog.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
-GIT_LOG_ALIAS="JOOBA";
+GIT_LOG_ALIAS=();
 GIT_LOG_ALIAS_PIECE="";
 
 function show_main_menu() {
@@ -10,18 +10,58 @@ function show_main_menu() {
     echo $'3. Exit';
 }
 
-function show_aliases_menu() {
+function show_pieces_menu() {
     echo $'\nWhat you want to add to your git log!\n';
-    echo $'1. Commit Hash (like 22e2792)';
+    echo $'1. Commit hash';
+    echo $'2. Tree hash';
+    echo $'3. Parent hash';
+    echo $'4. Parent hash';
+    echo $'5. Author name';
+    echo $'6. Author email';
+    echo $'7. Author email';
+    echo $'8. Author date';
+    echo $'9. Relative author date';
+    echo $'10. Commiter name';
+    echo $'11. Commiter email';
+    echo $'12. Commiter date';
+    echo $'13. Relative commiter date';
+    echo $'14. Message';
+    echo $'15. Back';
+    echo $'16. Exit';
+}
+
+function show_colors_menu() {
+    echo $'';
+}
+
+function show_divider_menu() {
+    echo $'';
+}
+
+function handle_piece_choice() {
+    echo $'';
 }
 
 while [ "$MENU_CHOISE" != 3 ];
     do
-        if [[ "$MENU_CHOISE" == 1 ]]; then
-            show_aliases_menu;
-            read LOG_PIECE;
-            echo "$LOG_PIECE";
-        fi
+        case $MENU_CHOISE in
+        1)
+            unset MENU_CHOISE;
+            while [ "$PIECE_MENU_CHOISE" != 15 ];
+                do
+                    show_pieces_menu;
+                    read PIECE_MENU_CHOISE;
+                done
+            ;;
+        2)
+            unset MENU_CHOISE;
+            echo "Take your hug <3"
+            ;;
+        3)
+            unset MENU_CHOISE;
+            exit
+            ;;
+        esac
         show_main_menu;
         read -p $'\nTell me what you want\n' MENU_CHOISE;
     done
